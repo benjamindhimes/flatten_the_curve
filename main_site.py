e520918f8a86dfeb95ed67d0c1c619b04e01b161
bdhimes <benjamindhimes@gmail.com>
2020-07-07 18:57:12 -0400

    Added cases data that gets mapped against the death rate.

--- main_site.py
+++ main_site.py
@@ -6,6 +6,8 @@ import os
 import pandas as pd
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import quart
 import requests
 import yaml
@@ -34,14 +36,21 @@ async def death_chart():
     county = quart.request.args.get("county")
     if not county or county not in COUNTIES:
         return quart.abort(404)
-    county_data_ = await get_county_data(county)
-    county_data_df = pd.DataFrame([convert_ts_in_obj(x["attributes"]) for x in county_data_["features"]])
+    county_death_data, county_cases_data = await asyncio.gather(
+        get_county_data(county),
+        get_county_data(county, "cases")
+    )
+    # county_data_df = pd.DataFrame([convert_ts_in_obj(x["attributes"]) for x in county_death_data["features"]])
     death_average = Decimal(
         Decimal(
-            sum(x["attributes"]["deaths"] for x in county_data_["features"][-7:])
+            sum(x["attributes"]["deaths"] for x in county_death_data["features"][-7:])
         ) / 7
     ).quantize(Decimal("0.01"))
-    plot = render_plot(county_data_df, county)
+    plot = render_complex_plot(
+        [convert_ts_in_obj(x["attributes"]) for x in county_death_data["features"]],
+        [convert_ts_in_obj(x["attributes"]) for x in county_cases_data["features"]],
+        county
+    )
     return await quart.render_template("display.html",
                                        graph=quart.Markup(plot.to_html()),
                                        county=county,
@@ -53,14 +62,37 @@ async def not_found(_):
     return "The requested data was not found."
 
 
-async def get_county_data(county_name: str):
-    url = (f"https://services2.arcgis.com/xtuWQvb2YQnp0z3F/arcgis/rest/services/Covid_Deaths_County/FeatureServer/0/"
-           f"query?f=json&where=county%3D%27{county_name}%27"
-           f"&returnGeometry=false"
-           f"&spatialRel=esriSpatialRelIntersects"
-           f"&outFields=ObjectId%2Cdeaths%2Cdate&orderByFields=date%20asc"
-           f"&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true")
-    r = requests.get(url)
+async def get_county_data(county_name: str, data_type: str = "deaths"):
+    case = {
+        "deaths": (
+            "Covid_Deaths_County",
+            {"f": "json",
+             "where": f"county='{county_name}'",
+             "returnGeometry": False,
+             "spatialRel": "esriSpatialRelIntersects",
+             "outFields": "ObjectId,deaths,date",
+             "orderByFields": "date asc",
+             "resultOffset": 0,
+             "resultRecordCount": 32000,
+             "resultType": "standard",
+             "cacheHint": True}),
+        "cases": (
+            "Covid_Cases_County",
+            {"f": "json",
+             "where": f"county='{county_name}'",
+             "returnGeometry": False,
+             "spatialRel": "esriSpatialRelIntersects",
+             "outFields": "ObjectId,cases,date,county",
+             "orderByFields": "date asc",
+             "resultOffset": 0,
+             "resultRecordCount": 32000,
+             "resultType": "standard",
+             "cacheHint": True}
+        )
+    }
+    service, params = case.get(data_type)
+    url = f"https://services2.arcgis.com/xtuWQvb2YQnp0z3F/arcgis/rest/services/{service}/FeatureServer/0/query"
+    r = requests.get(url, params=params)
     return r.json()
 
 
@@ -79,6 +111,29 @@ def render_plot(county_data: pd.DataFrame, county_name: str) -> plotly.graph_obj
                   x="date",
                   title=f"{county_name} County Death Data",
                   range_y=[0, 10] if max_single_day_deaths < 11 else [0, max_single_day_deaths])
+    return fig
+
+
+def render_complex_plot(death_data: list, cases_data: list, name: str):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(
+        go.Scatter(x=[_x["date"] for _x in death_data],
+                   y=[_y["deaths"] for _y in death_data],
+                   name="Deaths"),
+        secondary_y=False
+    )
+    fig.add_trace(
+        go.Scatter(x=[_x["date"] for _x in cases_data],
+                   y=[_y["cases"] for _y in cases_data],
+                   name="Cases"),
+        secondary_y=True
+    )
+    fig.update_layout(
+        title_text=f"{name.capitalize()} County Death/Cases Data"
+    )
+    fig.update_xaxes(title_text="Date")
+    fig.update_yaxes(title_text="Daily Deaths", secondary_y=False)
+    fig.update_yaxes(title_text="Daily Cases", secondary_y=True)
     return fig
 
 
